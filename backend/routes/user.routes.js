1e29e7b6704dce65efa3bea647d39ab984bc54fc
Rabah Ali Shah <muhammadrabah.ali46@gmail.com>
2024-04-01 07:44:03 +0500

    made changes to UI

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,6 +4,6 @@ import { getUsersForSidebar } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-router.get('/', protectedRoute,getUsersForSidebar);
+router.get('/', protectedRoute, getUsersForSidebar);
 
 export default router;
